31113d9f7752859f33e64529db0b0460063905a1
Tatsuya.Komeda <98158024+TatsuyaKomeda@users.noreply.github.com>
2022-03-09 17:19:04 +0900

    Initial commit

---
+++ src/providers/LoginUserProvider.tsx
@@ -0,0 +1,32 @@
+import React, {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useState
+} from "react";
+
+import { User } from "../types/api/user";
+
+type loginUser = User & { isAdmin: boolean };
+
+export type LoginUserContextType = {
+  loginUser: loginUser | null;
+  setLoginUser: Dispatch<SetStateAction<LoginUser | null>>;
+};
+
+export const LoginUserContext = createContext<LoginUserContextType>(
+  {} as LoginUserContextType
+);
+
+// ログインユーザー情報を保持するcontext
+export const LoginUserProvider = (props: { children: ReactNode }) => {
+  const { children } = props;
+  const [loginUser, setLoginUser] = useState<LoginUser | null>(null);
+
+  return (
+    <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
+      {children}
+    </LoginUserContext.Provider>
+  );
+};
